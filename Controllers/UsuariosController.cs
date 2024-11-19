eb1f0c6f6bf73e8b599485cb5c56e0f6a1d594f7
cracsi <98497136+cracsi@users.noreply.github.com>
2024-11-19 00:12:13 -0500

    Add project files.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PruebaAPI_C.DB;
+
+namespace PruebaAPI_C.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+    }
+}
---
+++ DB/DbContext.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PruebaAPI_C.Entities;
+
+namespace PruebaAPI_C.DB
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
